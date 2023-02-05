6599dc41821f4b2f876a87dee622fca0dd036f92
jordanbolivarq <112904376+JordanBolivarQuintero@users.noreply.github.com>
2023-02-05 03:57:31 -0500

    ChangeToMain

---
+++ Assets/Scripts/Tool/Hook.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Hook : MonoBehaviour
+{
+    [SerializeField] float throwTime;
+    float timer;
+    GameObject target;
+    Transform canyon;
+    LineRenderer lineRenderer;
+
+    enum State
+    {
+        Thrown,
+        Hit,
+        PickingUp,
+        Destroy
+    }
+    State state;
+
+    void Start()
+    {
+        canyon = transform.parent;
+        gameObject.GetComponent<Rigidbody>().AddForce((AimPoint.instance.aimPoint - canyon.position).normalized * 20, ForceMode.VelocityChange);
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        state = State.Thrown;
+        timer = 0;
+    }
+    void Update()
+    {
+        lineRenderer.SetPosition(0, canyon.position);
+        lineRenderer.SetPosition(1, transform.position);
+
+        switch (state)
+        {
+            case State.Thrown:
+                Throw();
+                break;
+            case State.Hit:
+                OnHit();
+                break;
+            case State.PickingUp:
+                Comeback();
+                break;
+            case State.Destroy:
+                DestroyTarget();
+                break;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.isTrigger && (int)state < 1)
+        {
+            state = State.Hit;
+            target = other.gameObject;
+        }
+    }
+    void Throw()
+    {
+        timer += Time.deltaTime;
+        if (timer >= throwTime)
+        {
+            gameObject.GetComponent<Rigidbody>().Sleep();
+            state = State.PickingUp;
+        }
+    }
+    void OnHit()
+    {
+        if (target.gameObject.layer == 7)
+        {
+            target.transform.parent = transform;
+            target.GetComponent<Rigidbody>().Sleep();
+        }
+        else
+            target = null;
+
+        gameObject.GetComponent<Rigidbody>().Sleep();
+
+        state = State.PickingUp;
+    }
+    bool Return()
+    {
+        bool done = false;
+        if (transform.position != canyon.position)
+            transform.position = Vector3.MoveTowards(transform.position, canyon.position, 20 * Time.deltaTime);
+        else
+            done = true;
+        return done;
+    }
+    void Comeback()
+    {
+        if (Return())
+            state = State.Destroy;  
+    }
+    bool DropToInventory()
+    {
+        bool done = false;
+        if (target != null)
+        {           
+            target.transform.parent = null;
+            target.GetComponent<Rigidbody>().Sleep();
+            transform.parent.parent.gameObject.GetComponent<Tool>().SaveCollectable(target.GetComponent<Collectable>()); // refactory collectable
+            done = true;
+        }
+        else
+            done = true;
+        return done;
+    }
+    void DestroyTarget()
+    {
+        if (DropToInventory())
+        {
+            if (target != null)
+            {
+                target.SetActive(false);
+                target = null;
+            }
+            else
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Tool/Tool.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    
+    Vector3 aimPoint;
+    bool shooting;
+    float shootTimer;
+    Collectable currentCollectable;
+    GameObject hookInstance;
+
+    [SerializeField] InventoryManager inventory;
+    [SerializeField] Transform canyon; 
+    [SerializeField] GameObject hook;
+    [SerializeField] float fireRate, bulletSpeed;
+
+    private void Update()
+    {
+        aimPoint = AimPoint.instance.aimPoint - canyon.position;
+        ActionTool();
+    }
+    void ActionTool()
+    {
+        if (shooting)
+            FireRate();
+        else
+        {
+            Pull();
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            aimPoint = aimPoint.normalized;
+
+            currentCollectable = inventory.RemoveItem();//cambiar 
+
+            if (currentCollectable != null)
+            {
+                currentCollectable.gameObject.SetActive(true);
+                currentCollectable.transform.parent = null;
+                currentCollectable.transform.position = canyon.position;
+                currentCollectable.GetComponent<Rigidbody>().Sleep();
+
+                currentCollectable.GetComponent<Rigidbody>().AddForce(aimPoint.normalized * bulletSpeed, ForceMode.VelocityChange); 
+            }
+
+            shooting = true;
+        }
+    }
+
+    void Pull()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            aimPoint = aimPoint.normalized;
+
+            hookInstance = GameObject.Instantiate(hook,canyon); 
+
+            shooting = true;
+        }
+    }
+    void FireRate()
+    {
+        if (shootTimer > (1 / fireRate))
+        {
+            shooting = false;
+            shootTimer = 0;
+        }
+        else
+            shootTimer += Time.deltaTime;
+    }
+    public void SaveCollectable(Collectable collectable)
+    {
+        inventory.AddToInventory(collectable);
+    }
+}
+
