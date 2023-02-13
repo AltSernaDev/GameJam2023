052cc59a8a7e824c51d50bf61076d577ef36e58d
Santiago Lara <slm-902@hotmail.com>
2023-02-12 23:40:10 -0500

    Feature: Progression System pt1

---
+++ Assets/Scripts/Progress/AchievementDataBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AchievementDataBase : ScriptableObject
+{
+    public List<Achievement> achivements;
+}
---
+++ Assets/Scripts/Progress/AchievementDataBaseImporter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(AchievementDataBase))]
+public class AchievementDataBaseImporter : Editor
+{
+    AchievementDataBase dataBase;
+
+    private void OnEnable()
+    {
+        dataBase = target as AchievementDataBase;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Import Enum to file"))
+        {
+            ImportAchivement();
+        }
+
+    }
+    public void ImportAchivement()
+    {
+        string path = Path.Combine(Application.dataPath, "Achievements.cs");
+        string codeBody = "public enum Achievement\n{";
+        foreach (Achievement item in dataBase.achivements)
+        {
+            codeBody += item.id + ",";
+        }
+        codeBody += "\n}";
+        File.WriteAllText(path, codeBody);
+        AssetDatabase.ImportAsset("Assets/Scripts/Progress/Achievements.cs");
+    }
+}
